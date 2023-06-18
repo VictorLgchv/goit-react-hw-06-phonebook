507648a84f6e04bdcf09988edc95702fed3f2023
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-06-18 18:00:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <App />
+        <App />
         <GlobalStyle />
-        </PersistGate>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
